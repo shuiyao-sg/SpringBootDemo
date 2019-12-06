f0355b10a97d55b15e137047dccb44a31080ffe6
shuiyao <shuiyao@u.nus.edu>
2019-12-06 17:51:18 +0800

    Optimise import

--- src/main/java/com/example/demo/api/PersonController.java
+++ src/main/java/com/example/demo/api/PersonController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @RequestMapping("api/v1/person")
